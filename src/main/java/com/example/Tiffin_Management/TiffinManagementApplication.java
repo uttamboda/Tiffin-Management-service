3cd15a9d1363ce613b1bb2a187c8598d477fa5f1
Uttam Boda <up6440631@gmail.com>
2026-02-28 11:59:50 +0530

    terminal is clear not print anyone

--- src/main/java/com/example/Tiffin_Management/TiffinManagementApplication.java
+++ src/main/java/com/example/Tiffin_Management/TiffinManagementApplication.java
@@ -3,7 +3,10 @@ package com.example.Tiffin_Management;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
+@SpringBootApplication(excludeName = {
+		"org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration",
+		"org.springframework.boot.autoconfigure.security.servlet.UserDetailsServiceAutoConfiguration"
+})
 public class TiffinManagementApplication {
 
 	public static void main(String[] args) {
